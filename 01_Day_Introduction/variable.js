ede3c408484ebe045a51f0c1dd846e082d3032cb
omerfulin <omerfulin@gmail.com>
2022-11-18 14:15:40 +0300

    Day 01

--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -5,3 +5,15 @@ let country = "Türkiye"
 let city = "İstanbul"
 
 console.log(firstName,lastName,age,country,city)
+
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14;
+
+console.log(gravity,boilingPoint,PI)
+
+let name = "omer",
+    job = "Developer",
+    yas = 14
+
+console.log(name,job,yas)
